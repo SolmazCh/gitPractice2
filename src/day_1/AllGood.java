bc6d012df60554fe35bac0273c31f3a806bdffd3
SolmazCh <64098166+SolmazCh@users.noreply.github.com>
2020-04-29 10:39:36 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -4,8 +4,6 @@ public class AllGood {
 
     public static void main(String[] args) {
 
-//for changes from github
-
-//just for practice I added this line from github
+//Hello Cybertek! //from Solmaz
     }
 }
